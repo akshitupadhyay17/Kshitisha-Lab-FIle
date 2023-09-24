77be5c51a96e9ad9d1ef43461b75f1b93c2de349
akshitupadhyay17 <139381971+akshitupadhyay17@users.noreply.github.com>
2023-09-24 21:16:53 +0530

    Update Experiment_2_1_graph.py

--- Kshitisha Lab File/Experiment_2_1_graph.py
+++ Kshitisha Lab File/Experiment_2_1_graph.py
@@ -8,21 +8,18 @@ def linear_search(arr, target):
     return -1
 
 def measure_time(n, target):
-    elements = list(range(1, n + 1))  # Create an array of n elements (1, 2, 3, ..., n)
+    elements = list(range(1, n + 1))
     start_time = time.time()
     found_index = linear_search(elements, target)
     end_time = time.time()
     elapsed_time = end_time - start_time
     return n, elapsed_time
 
-# Initialize variables
 array_sizes = []
 elapsed_times = []
 
-# Input the number of searches
 num_searches = int(input("Enter the number of searches: "))
 
-# Input the search targets and measure time for different array sizes
 for _ in range(num_searches):
     n = int(input("Enter the number of elements in the array: "))
     target = int(input("Enter the target element to search for: "))
@@ -31,7 +28,6 @@ for _ in range(num_searches):
     array_sizes.append(array_size)
     elapsed_times.append(elapsed_time)
 
-# Create a line plot with two separate lines
 plt.figure(figsize=(10, 6))
 plt.plot(array_sizes, label='Number of Elements', marker='o', linestyle='-')
 plt.plot(elapsed_times, label='Elapsed Time (seconds)', marker='o', linestyle='-')
