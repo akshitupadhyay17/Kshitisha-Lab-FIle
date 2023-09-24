d7bbac65a56f6b5fa188c05a913339844d9a74d0
akshitupadhyay17 <139381971+akshitupadhyay17@users.noreply.github.com>
2023-09-24 20:45:23 +0530

    Update Experiment_2_2.c

--- Kshitisha Lab File/Experiment_2_2.c
+++ Kshitisha Lab File/Experiment_2_2.c
@@ -1,31 +1,52 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include <time.h>
 
-void bubbleSort(int arr[], int n) {
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = 0; j < n - i - 1; j++) {
-            if (arr[j] > arr[j+1]) {
-                int temp = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = temp;
-            }
+int binarySearch(int arr[], int n, int target) {
+    int left = 0;
+    int right = n - 1;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+
+        if (arr[mid] == target) {
+            return mid; // Return the index where the element is found
+        } else if (arr[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
         }
     }
+
+    return -1; // Return -1 if the element is not found
 }
 
 int main() {
-    int elements[] = {64, 34, 25, 12, 22, 11, 90};
-    int n = sizeof(elements) / sizeof(elements[0]);
+    int n;
+
+    printf("Enter the number of elements in the sorted array: ");
+    scanf("%d", &n);
+
+    int elements[n];
+
+    printf("Enter %d sorted elements separated by spaces: ", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &elements[i]);
+    }
+
+    int target;
+    printf("Enter the target element to search for: ");
+    scanf("%d", &target);
 
     clock_t start_time = clock();
-    bubbleSort(elements, n);
+    int foundIndex = binarySearch(elements, n, target);
     clock_t end_time = clock();
 
-    printf("Sorted array: ");
-    for (int i = 0; i < n; i++) {
-        printf("%d ", elements[i]);
+    if (foundIndex != -1) {
+        printf("Element %d found at index %d in the array.\n", target, foundIndex);
+    } else {
+        printf("Element %d not found in the array.\n", target);
     }
-    printf("\n");
 
     double time_taken = ((double)(end_time - start_time)) / CLOCKS_PER_SEC;
     printf("Time taken: %f seconds\n", time_taken);
